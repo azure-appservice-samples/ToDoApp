8d94036366aae7a7315ce807b5fdb286eb1c6417
Cory Fowler <cfowler@microsoft.com>
2015-04-13 21:08:33 -0700

    initial commit

---
+++ MultiChannelToDo/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MultiChannelToDo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new { id = RouteParameter.Optional }
+            );
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*", "*"));
+        }
+    }
+}
---
+++ MultiChannelToDo/Models/ToDoItem.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MultiChannelToDo.Models
+{
+    [JsonObject]
+    public class TodoItem
+    {
+        public TodoItem()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTimeOffset.UtcNow;
+            UpdatedAt = DateTimeOffset.UtcNow;
+        }
+
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+        public DateTimeOffset? CreatedAt { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
