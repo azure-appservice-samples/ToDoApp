b4b4561c1821d6f0dbe0ae4d13ead9105c86cf93
Cory Fowler <cfowler@microsoft.com>
2015-04-27 10:19:37 -0700

    fixing the issue with Seed getting run on each update

--- src/MultiChannelToDo/Migrations/Configuration.cs
+++ src/MultiChannelToDo/Migrations/Configuration.cs
@@ -16,10 +16,10 @@ namespace MultiChannelToDo.Migrations
 
         protected override void Seed(MultiChannelToDo.Models.MultiChannelToDoContext context)
         {
-            context.ToDoItems.AddRange(new List<TodoItem> {
+            context.ToDoItems.AddOrUpdate(
             new TodoItem
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = "6DF176EA-E0D4-4C04-ABFA-8533CB4680CE",
                 Text = "Build Sample",
                 Complete = false,
                 Deleted = false,
@@ -28,7 +28,7 @@ namespace MultiChannelToDo.Migrations
             },
             new TodoItem
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = "50DF07C4-A769-4084-BA5B-F4F5FBF28688",
                 Text = "Create ARM Template",
                 Complete = false,
                 Deleted = false,
@@ -37,7 +37,7 @@ namespace MultiChannelToDo.Migrations
             },
             new TodoItem
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = "585A4B75-CB85-44E8-BF50-B49AB6965632",
                 Text = "Publish to Azure",
                 Complete = false,
                 Deleted = false,
@@ -46,13 +46,13 @@ namespace MultiChannelToDo.Migrations
             },
             new TodoItem
             {
-                Id = Guid.NewGuid().ToString(),
+                Id = "A23A2F75-CC52-4F1A-B459-4F8AB019CB4D",
                 Text = "Make Money",
                 Complete = false,
                 Deleted = false,
                 UpdatedAt = DateTimeOffset.UtcNow,
                 CreatedAt = DateTimeOffset.UtcNow
-            }});
+            });
         }
     }
 }
