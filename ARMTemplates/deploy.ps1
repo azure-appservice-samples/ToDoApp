8ce7980a6149c22c450bbe61fe530c4132d5ebc5
cephalin <cephalin@microsoft.com>
2015-06-24 17:42:27 -0500

    prompt for credentials

--- ARMTemplates/deploy.ps1
+++ ARMTemplates/deploy.ps1
@@ -5,6 +5,49 @@
 	[string] $Branch = "master"
 )
 
+# Wait for a web app deployment to finish
+# Get more useful functions at https://github.com/davidebbo/AzureWebsitesSamples/blob/6780a548a523cdccd1dfd66f442a7995bbb29898/PowerShell/HelperFunctionsTest.ps1
+Function WaitOnDeployment($ResourceGroupName, $SiteName) 
+{ 
+	Write-Host -NoNewline "Waiting until the deployment for $SiteName is done..."
+
+	#Check if staging slot is specified
+	if ($SiteName -match "/Staging")
+	{
+		$resourceType = "Microsoft.Web/sites/slots/Deployments"
+	}
+	else
+	{
+		$resourceType = "Microsoft.Web/sites/Deployments"
+	}
+
+	While ($true) 
+	{ 
+		$deployments = Get-AzureResource `
+						-ResourceGroupName $ResourceGroupName `
+						-ResourceType $resourceType `
+						-Name $SiteName `
+						-OutputObjectFormat New `
+						-ApiVersion 2015-06-01 
+
+		if ($deployments) 
+		{
+			$latestDeployment = $deployments[0].Properties 
+			if (-not $latestDeployment.Complete) 
+			{
+				Write-Host -NoNewline "."
+			} 
+			else
+			{
+				Write-Host "Complete!"
+				break
+			}
+		} 
+	} 
+
+} 
+
+# MAIN
 $start = get-date
 
 if (!(Test-Path ".\$TemplateFile")) 
@@ -22,12 +65,14 @@ else
 		Import-Module Azure
 	}
 
+	[System.Console]::Clear()
+
 	#Use ARM cmdlets
 	Switch-AzureMode -Name AzureResourceManager
 
 	#Random
 	#Used to randomize the names of the resources being created to avoid conflicts
-	$Random = [system.guid]::NewGuid().tostring().substring(0,5)
+	$Random = [system.guid]::NewGuid().tostring().substring(0,5) + $Branch.ToLower()
 
 	#Resource Group Properties
 	$RG_Name = "ToDoApp${Random}-group"
@@ -43,14 +88,15 @@ else
 		
 	Write-Host "Creating Resource Group, App Service Plan, Web Apps and SQL Database..." -ForegroundColor Green 
 	try 
-	{ 
+	{
+		#Missing parameters in the parameters file, such as sqlServerAdminLogin and sqlServerAdminPassword, will be
+		#prompted automatically and securely
 		New-AzureResourceGroup -Verbose `
 			-name $RG_Name `
 			-location $RG_Location `
 			-TemplateFile ".\$TemplateFile" `
 			-TemplateParameterFile ".\temp.json" `
 			-ErrorAction Stop
-			
 	}
 	catch 
 	{
@@ -60,13 +106,33 @@ else
 
 	Remove-Item .\temp.json | Out-Null
 
+	#Wait for Kudu deployment to complete and launch the deployed web application
+	If($TemplateFile -match "ProdAndStage.json")
+	{
+		WaitOnDeployment $RG_Name "ToDoApp${Random}/Staging"
+		WaitOnDeployment $RG_Name "ToDoApp${Random}Api/Staging"
+
+		Switch-AzureMode -Name AzureServiceManagement -WarningAction SilentlyContinue
+		Show-AzureWebsite -Name "ToDoApp$Random" -Slot Staging
+	}
+	else
+	{
+		WaitOnDeployment $RG_Name "ToDoApp${Random}"
+		WaitOnDeployment $RG_Name "ToDoApp${Random}Api"
+
+		Switch-AzureMode -Name AzureServiceManagement -WarningAction SilentlyContinue
+		Show-AzureWebsite -Name "ToDoApp$Random"
+	}
+
 	Write-Host "-----------------------------------------"  -ForegroundColor Green 
 	Write-Host $file "execution done"  -ForegroundColor Green 
 	[System.Console]::Beep(400,1500)
 	
+
 	$end = get-date
 
 	write-host "Start= " $start.Hour ":" $start.Minute ":" $start.Second
 	write-host "End= " $end.Hour ":" $end.Minute ":" $end.Second
+
 	pause
 }
